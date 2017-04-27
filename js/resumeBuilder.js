493dc2762b8b17fc09a76bc806907d5d320cb965
Brandon Batten <Brandon Batten>
2017-04-26 23:08:12 -0400

    Add bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,26 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+bio = {
+  'name': "Brandon Batten",
+  'role': "Front-End Web Developer",
+  'contacts': {
+    'mobile': '919-631-9999',
+    'email': 'ba_batten@live.com',
+    'github': 'ba-batten',
+    'location': 'Durham, NC'
+  }
+
+};
+
+object = {
+
+};
+
+object = {
+
+};
+
+object = {
+
+};
